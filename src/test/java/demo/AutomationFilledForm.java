640cdf8a4bf4acab303cdec584f0dabae4ea4364
deolgal <86726128+deolgal@users.noreply.github.com>
2023-11-24 15:23:24 +0300

    change test

--- src/test/java/demo/AutomationFilledForm.java
+++ src/test/java/demo/AutomationFilledForm.java
@@ -2,7 +2,6 @@ package demo;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -29,15 +28,20 @@ public class AutomationFilledForm {
         String address = "Mashinostroiteley 5-150";
 
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(Condition.text("Student Registration Form"));
         //fill form
         $(byText("Female")).click();
+        //$("#gender-radio-2").parent().click(); тоже допустимо, parent используется из-за перекрытия элемента родительским
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
         $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirth div.react-datepicker-wrapper").click();
         $(".react-datepicker__month-select").click();
+
         $(byText("October")).click();
+        // или при помощи команды selectOptions("October) или при при помощи selectValue(9) - там список сделан при помощи select
+
         $(".react-datepicker__year-select").click();
         $(byText("1994")).click();
         $(".react-datepicker__day--012").click();
@@ -48,15 +52,27 @@ public class AutomationFilledForm {
 
         $("#uploadPicture").uploadFile(new File("src/test/resources/qa-2-min.png"));
         $("#currentAddress").setValue(address);
-        $("#react-select-3-input").setValue("Haryana").pressEnter();
-        $("#react-select-4-input").setValue("Panipat").pressEnter();
 
-        /// hide overlay banners
+//нужно выполнить в начале из-за наличия банеров на странице мешающих нажать на элемент
+
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
-        $(".btn-primary").click();
-        $(".table-responsive").shouldHave(Condition.text(firstName),Condition.text(lastName),Condition.text(address),Condition.text(email),Condition.text("qa-2-min.png"),Condition.text("Haryana Panipat"),Condition.text(phoneNumber));
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+
+        //такой вариант тоже возможен, но первый лучше
+        //$("#react-select-3-input").setValue("Haryana").pressEnter();
+        //$("#react-select-4-input").setValue("Panipat").pressEnter();
+
+
+        $("#submit").click();
+        $(".modal-dialog").shouldBe(Condition.appear);
+
+        $(".table-responsive").shouldHave(Condition.text(firstName),Condition.text(lastName),Condition.text(address),Condition.text(email),Condition.text("qa-2-min.png"),Condition.text("Haryana Karnal"),Condition.text(phoneNumber));
 
 
 
